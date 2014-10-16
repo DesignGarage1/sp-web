08a2471a6fb956fefe33b77e25e6631baf44cb67
kiennt <trungkien2288@gmail.com>
2014-10-16 16:05:23 +0700

    Update upload link

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,8 +5,7 @@ import FileObject from 'web/libs/file';
 
 export default Ember.Controller.extend(UploadMixin, {
     isDone: false,
-       /*uploadUrl: 'http://bns-proxy.herokuapp.com/upload',*/
-    uploadUrl: 'http://localhost:3000/upload',
+    uploadUrl: 'http://bns-proxy.herokuapp.com/upload',
     actions: {
       done: function() {
         this.set('isDone', true);
