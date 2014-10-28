516d9b5239a80e828331842db5ee804533b757ca
kiennt <trungkien2288@gmail.com>
2014-10-28 20:16:31 +0700

    Change style of index.js

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,11 +3,11 @@ import UploadMixin from 'web/libs/upload';
 
 
 export default Ember.Controller.extend(UploadMixin, {
-    isDone: false,
-    uploadUrl: 'http://sp-proxy-0.herokuapp.com/upload',
-    actions: {
-      done: function() {
-        this.set('isDone', true);
-      },
-    }
+  isDone: false,
+  uploadUrl: 'http://sp-proxy-0.herokuapp.com/upload',
+  actions: {
+    done: function() {
+      this.set('isDone', true);
+    },
+  }
 });
